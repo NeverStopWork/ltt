9fe115ce8aa37c59ff297cc818a31971556d93bc
NeverStopWork <905004116@qq.com>
2018-03-20 17:01:11 +0800

    2
    2

---
+++ config/params.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'adminEmail' => 'admin@example.com',
+    'WebHost'    => 'http://39.106.28.143',
+    'api.log.path' => '../runtime/project-log',
+    'img_path'      => '/var/www/ltt/web/img',
+    'web_path'      => '/var/www/ltt/web',
+    'img_url'    => 'http://39.106.28.143/img',
+];
